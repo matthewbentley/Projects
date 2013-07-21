8ef9c58b18a5006dddd2f6f20c030b2f9ea5d262
Matthew Bentley <matthew.t.bentley@gmail.com>
2013-07-20 20:22:56 -0700

    Finished documentation header for pi.lisp, tested it and it works perfectly

--- numbers/pi.lisp
+++ numbers/pi.lisp
@@ -1,4 +1,5 @@
 ;;;;Calculates the value of pi to an arbitrary number of digets
+;;;;Takes the number of decimals (after the "3.") from the first command line argument
 ;;;;Using Machin's formula (pi/4 = 4 arccot(5) - arccot(293))
 ;;;;To calculate arccot: arccot(x)= 1/x - 1/3x^3 + 1/5x^5 - 1/7x^7 ...
 
@@ -30,7 +31,16 @@
 	 (betterpi (* 4 (- (* 4 (arccot 5 unity)) (arccot 239 unity)))))
     (floor (/ betterpi (expt 10 10)))))
 
+(defun piformat (how-many input)
+  "Formats a number (like 31415) to pi (3.14159)"
+  (concatenate 'string (subseq input 0 1) "." (subseq input 1 (+ 1 how-many))))
+
 (when (equal (second *posix-argv*) NIL)
+  (print "Please leave a command line value for the number of digits you want")
   (quit))
 (defvar *answer* (parse-integer (second *posix-argv*)))
-(print (pidigits *answer*))
+
+(print
+ (piformat *answer*
+  (write-to-string
+   (pidigits *answer*))))
