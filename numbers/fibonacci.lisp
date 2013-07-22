e19e2995caa6711f8323747afa510f64c783ee35
Matthew Bentley <matthew.t.bentley@gmail.com>
2013-07-21 20:06:05 -0700

    Finished fibonacci.lisp.  It works. (Might add ability to only print last value, it currently prints them all.)

--- numbers/fibonacci.lisp
+++ numbers/fibonacci.lisp
@@ -1,3 +1,6 @@
+;;;;Generates up to either the nth number in the fibonacci sequence or the last number less than n
+;;;;Call with the letter n for nth, or m to stop at a max value, followed by a value
+
 (defun should-continue (nth nth-supplied-p stop stop-supplied-p count curr-num)
   "NIL if it should go on until the next loop, T if not"
   (if
@@ -15,4 +18,15 @@
       ((should-continue nth nth-supplied-p stop stop-supplied-p count x))
     (print x)))
 
-(genfib :stop 50 :nth 10)
+(when (not (or (equal (second *posix-argv*) "m") (equal (second *posix-argv*) "n")))
+  (print "Please use either n for nth or m for max value as the first argument")
+  (quit))
+
+(when (equal (third *posix-argv*) NIL)
+  (print "Please enter a number for the second argument")
+  (quit))
+(cond ((equalp (second *posix-argv*) "m")
+       (genfib :stop (parse-integer (third *posix-argv*))))
+      ((equalp (second *posix-argv*) "n")
+       (genfib :nth (parse-integer (third *posix-argv*))))
+      (T (print "Something went wrong :O")))
